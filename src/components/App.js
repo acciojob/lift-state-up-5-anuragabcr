f26d301a0b5bd6368660349f3117460f7cebdb57
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-11 10:40:12 +0530

    added p tag

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <h1>Parent Component</h1>
       {isLoggedIn ? (
-        <div>You are logged in!</div>
+        <p>You are logged in!</p>
       ) : (
         <Login onLogin={handleLogin} />
       )}
